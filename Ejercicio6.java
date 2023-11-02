72d64efc04b15cd75748c049f8f0184c499a64ba
Ultimate-Truth-Seeker <96703353+Ultimate-Truth-Seeker@users.noreply.github.com>
2023-11-01 21:17:06 -0600

    1

--- Ejercicio6.java
+++ Ejercicio6.java
@@ -1,19 +1,50 @@
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
+/**
+ * Programa para manejo de dsipositivos
+ * NOTA: los csv que lee y escribe llevan un entero en la primera columan que representa el tipo de dispositivo. Este dato solo es de referencia, no se añade a la memoria del programa
+ * @author Ultimate.Truth-Seeker
+ * @version 1 nov 2023
+ */
 public class Ejercicio6 {
     public static void main(String[] args) {
-        List<DispositivoElectronico> dispositivos = new ArrayList<>();
-        // TODO: implementar csv
-        dispositivos.add(new Telefono(false, "xd"));
-
+        List<DispositivoElectronico> dispositivos = new ArrayList<>();// Listado de dispositivos
         boolean menu = true;
         Scanner s = new Scanner(System.in);
-        while (menu) {
+        while (menu) {// menú de opciones
+            System.out.println("Bienvenido, ingrese una opción:\n1.Añadir dispositivo\n2.Ver información de cada dispositivo\n3.Validar elementos encendidos\n4.Cargar archivo csv con datos\n5.Guardar datos en archivo csv\n6.Salir");
             int op = s.nextInt();
             switch (op) {
-                case 1:
+                case 1:// añadir un nuevo dispositivo
+                    System.out.println("Ingrese 1 si el dispositivo está encendido y 0 si está apagado");
+                    int choice = s.nextInt();
+                    while (choice < 0 && choice > 1){
+                        choice = s.nextInt();
+                    }
+                    boolean status = false;
+                    if (choice == 1) {
+                        status = true;
+                    }
+                    System.out.println("Ingrese 1 si el dispositivo es un teléfono y 0 si es una computadora");
+                    choice = s.nextInt();
+                    while (choice < 0 && choice > 1){
+                        choice = s.nextInt();
+                    }
+                    s.nextLine();
+                    if (choice == 1) {
+                        System.out.println("Ingrese el modelo del teléfono: ");
+                        String modelo = s.nextLine();
+                        dispositivos.add(new Telefono(status, modelo));
+                    } else if (choice == 0) {
+                        System.out.println("Ingrese la marca de la computadora: ");
+                        String marca = s.nextLine();
+                        dispositivos.add(new Computadora(status, marca));
+                    }
+                    break;
+                case 2:// ver lista de dispositivos
                     System.out.println("Listado de dispositivos:");
                     for (DispositivoElectronico d : dispositivos) {
                         if (d.getClass() == Telefono.class) {
@@ -25,7 +56,7 @@ public class Ejercicio6 {
                         }
                     }
                     break;
-                case 2:
+                case 3:// verificar dispositivos encendidos
                     System.out.println("Elementos encendidos: ");
                     for (DispositivoElectronico d: dispositivos) {
                         if (d.isTurnedOn() == true) {
@@ -39,7 +70,47 @@ public class Ejercicio6 {
                         }
                     }
                     break; 
-                case 3:
+                case 4:// Cargar de archivo .csv
+                    System.out.println("Escriba el nombre del archivo csv a cargar:");
+                    s.nextLine();
+                    String path = s.nextLine();
+                    try (Scanner rd = new Scanner(new File(path))) {
+                        while (rd.hasNextLine()) {
+                            Scanner ln = new Scanner(rd.nextLine());
+                            ln.useDelimiter(",");
+                            int type = Integer.parseInt(ln.next());
+                            boolean on = Boolean.parseBoolean(ln.next());
+                            String m = ln.next();
+                            if (type == 1) {
+                                dispositivos.add(new Telefono(on, m));
+                            } else if (type == 0) {
+                                dispositivos.add(new Computadora(on, m));
+                            }
+                        }
+                        System.out.println("Lectura exitosa");
+                    } catch (Exception e) {
+                        System.out.println("Error en la lectura. Revise el nombre y la integridad del archivo.");
+                    }
+                    break;
+                case 5:// Guardar en archivo .csv
+                    System.out.println("Ingrese el nombre del archivo a escribir. La extensión .csv se añade automáticamente.");
+                    s.nextLine();
+                    path = s.nextLine();
+                    try (FileWriter wr = new FileWriter(new File(path + ".csv"))) {
+                        for (DispositivoElectronico d : dispositivos) {
+                            if (d.getClass() == Telefono.class) {
+                                Telefono t = (Telefono) d;
+                                wr.write("1,"+d.isTurnedOn()+","+t.getModelo());
+                            } else if (d.getClass() == Computadora.class) {
+                                Computadora c = (Computadora) d;
+                                wr.write("0,"+d.isTurnedOn()+","+c.getMarca());
+                            }
+                        }
+                    } catch (Exception e) {
+                        System.out.println("Error en la escritura");
+                    }
+                    break;
+                case 6:// Salir
                     menu = false;
                     break;
                 default:
